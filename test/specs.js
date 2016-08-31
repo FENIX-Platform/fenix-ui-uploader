7d6172f749c01c4b7940663e1ce25bbfbfb6bf71
Daniele Salvatore <daniele.salvatore@fao.org>
2016-08-31 18:05:09 +0200

    Intial commit (!)

---
+++ test/specs.js
@@ -0,0 +1,10 @@
+/*global describe, it, require*/
+var expect = require("chai").expect;
+var Component = require("../src/js/index");
+
+describe("Pivotator Utils", function () {
+
+    it("should be not null", function () {
+        expect(Component).to.be.not.null;
+    });
+});
